5181f67df7737fc8e51273c1742599e75dae6183
Manasa-bg-au9 <manasabg2993@gmail.com>
2020-09-26 23:35:30 +0530

    Swiggy Project

--- swiggy/mainPage.py
+++ swiggy/mainPage.py
@@ -29,21 +29,26 @@ class Swiggy():
     def mainMenu(self):
         while True:                                                 #Repeat Menu until stops.
             print("\n")
-            print("*" * 36 + "WELCOME TO SWIGGY" + "*" * 36 )       
+            print("*" * 36 + "SWIGGY" + "*" * 36 )       
             print("\n"*2)
             print("*" * 36 + "MAIN MENU" + "*" * 36 + "\n"              #Design for Main Menu
-                    "\t(O) ORDER FOOD\n"                                #"*" * 31 means, write (*) 31 times.
-                    "\t(J) OTHER SERVICES\n"
-                    "\t(E) EXIT\n"+
+                    "\t[P] PLACE ORDER\n"                                #"*" * 31 means, write (*) 31 times.
+                    "\t[O] OTHER SERVICES\n"
+                    "\t[S] SEARCH ITEM\n"
+                    "\t[Q] QUIT\n"+
                     "-" * 89)
 
-            input_1 = str(input("PLEASE SELECT YOUR OPERATION: ")).upper()   #Input, have to choose operation. Make everything UPPER symbol.
-            if (input_1 == "O"):                                             #If input is 'O' 
+            input_1 = str(input("PLEASE SELECT YOUR OPTION: ")).upper()   #Input, have to choose operation. Make everything UPPER symbol.
+            if (input_1 == "P"):                                             #If input is 'O' 
                 self.orderFood()                                              #Start Order Menu function.
                 break
-            elif (input_1 == "J"):                                           #If input is 'J'
+            elif (input_1 == "O"):                                           #If input is 'J'
                 self.joinServices()
-            elif (input_1 == "E"):
+                break
+            elif (input_1 == "S"):
+                self.search_Menu()
+                break
+            elif (input_1 == "Q"):
                 print("*" * 36 + "THANK YOU" + "*" * 36 + "\n")             #Good bye comment.
                 break
             else:
@@ -52,26 +57,28 @@ class Swiggy():
     def orderFood(self):                                                    # While looping to keep menu alive
         while True:
             print("\n" * 1)
-            print("#" * 36,"ORDER FOOD","#" * 36)
+            print("#" * 36,"PLACE ORDER","#" * 36)
             print("PLEASE SELECT THE RESTAURANT OF YOUR CHOICE: \n")        #Selecting the restaurant
             
             for i in range (0,len(self.restaurant_dict)):                   #Iterating over the list
-                print("(",i+1,")"," ",(list(self.restaurant_dict.keys())[i]),sep='')
+                print("[",i+1,"]"," ",(list(self.restaurant_dict.keys())[i]),sep='')
                 print()
             print()
-            print("(M) BACK TO MAIN MENU","\t" * 5,"(S) SEARCH FOR DISH / RESTAURANT") 
             print("-" * 89)
-            input_2 = input("PLEASE SELECT YOUR OPERATION: ")              #Options Handling
+            print("[B] BACK TO MAIN MENU","\t" * 5,"[S] SEARCH FOR ITEMS") 
+            print("-" * 89)
+            input_2 = input("PLEASE SELECT YOUR OPTION: ")              #Options Handling
 
             input_check = []
-            if input_2.upper() == "M":                                     #If input is 'M'
+            if input_2.upper() == "B":                                     #If input is 'M'
                 self.mainMenu()                                             #Back to main menu
                 break
             elif input_2.upper() == "S":                                   #If input is 'S'
-                print("\n" * 2)
-                print("SEARCH MENU")                                        #go to Search Menu
+                self.search_Menu()
+                # print("\n" * 2)
+                # print("SEARCH MENU")                                        #go to Search Menu
                 break    
-            elif input_2 != "M" and input_2 != "S":
+            elif input_2 != "B" and input_2 != "S":
                 for i in input_2:
                     if ord(i) >= 48 and ord(i) <= 57:
                         input_check.append("True")
@@ -89,7 +96,7 @@ class Swiggy():
     
     def restaurant_menu(self,res):
         if self.restaurant_dict[res] > 2:
-            print("SORRY WE ARE CURRENTLY NOT ACCEPTING ANY ORDERS FOR THIS RESTAURENT!")
+            print("SORRY!!!! CURRENTLY THIS RESTAURANT IS NOT ACCEPTING ORDER!")
             self.orderFood()
         else:
             dish_input_food_list = []
@@ -105,16 +112,16 @@ class Swiggy():
                     i = 0
                     for k,v in self.menu_restaurant[res].items():
                         i += 1
-                        print("(",i,")"," ",k,"\t"*7,v,sep="")
+                        print("[",i,"]"," ",k,"\t"*7,v,sep="")
                     print("-"*89)
-                    print("(V) VIEW CART","\t"*7,"(B) BACK TO ORDER FOOD PAGE")
+                    print("[C] GO TO CART","\t"*7,"[B] BACK TO ORDER FOOD PAGE")
                     print("-"*89)
                     dish_input = input("PLEASE SELECT DISH OF YOUR CHOICE: ")
                 num_input_check = []
                 if dish_input.upper() == "B":
                     self.orderFood()
                     break
-                elif dish_input.upper() == "V":
+                elif dish_input.upper() == "C":
                     print("\n"*2)
                     print("#"*39,"YOUR CART","#"*39)
                     if dish_input_food_list != [] and dish_input_price_list != [] and dish_quantity_list != []:
@@ -124,7 +131,7 @@ class Swiggy():
                         print("\n"*2)
                         print("\t"*4,"YOUR CART IS EMPTY!!")
                         continue
-                elif dish_input != "V" or dish_input != "B":
+                elif dish_input != "C" or dish_input != "B":
                     for i in dish_input:
                         if ord(i) >= 48 and ord(i) <= 57:
                             num_input_check.append("True")
@@ -137,7 +144,7 @@ class Swiggy():
                         dish_input = int(dish_input)
                         if dish_input <= len(self.menu_restaurant[res].items()):
                             try:
-                                dish_quantity = int(input("PLEASE SELECT THE QUANTITY FOR THE DISH SELECTED: "))
+                                dish_quantity = int(input("PLEASE SELECT THE QUANTITY: "))
                                 dish_quantity_list.append(dish_quantity)
                             except Exception:
                                 print("\n" + "ERROR: Invalid Input. Try again!")
@@ -165,13 +172,12 @@ class Swiggy():
                 total_price.append((dish_quantity_list[j])*(dish_input_price_list[j]))
                 j += 1
             print("\n")
-            print("-"*89)
             print("ITEMS","\t"*5,"QUANTITY","\t"*4,"PRICE")
             print("-"*89)
             i = 0
             sr_no = 1
             while i < len(dish_input_food_list):
-                print("(",sr_no,")"," ",dish_input_food_list[i],"\t","*","\t"*2, dish_quantity_list[i],\
+                print("[",sr_no,"]"," ",dish_input_food_list[i],"\t","*","\t"*2, dish_quantity_list[i],\
                     "\t"*5,total_price[i],sep="")
                 i += 1
                 sr_no += 1
@@ -182,11 +188,11 @@ class Swiggy():
                 discount_total_price = float(sum(total_price)) - (15*(sum(total_price))/100)
                 print("TOTAL","\t"*10,(discount_total_price))
             print("-"*89)
-            print("(P) PAYMENT","\t"*8,"(B) BACK TO MENU")
+            print("[P] PAYMENT","\t"*8,"[B] BACK TO MENU")
             print("-"*89)    
             cart_input = input("PLEASE SELECT YOUR OPERATION: ")
             if cart_input.upper() == "B":
-                restaurent_Menu(res)
+                self.restaurant_menu(res)
                 break
             elif cart_input.upper() == "P":
                 self.payment(res)
@@ -201,16 +207,15 @@ class Swiggy():
             print("\n")
             print("SR NO","\t"*2,"MODES OF PAYMENT")
             print("-"*89)
-            print("(1)","\t"*2,"CREDIT CARD")
-            print("(2)","\t"*2,"DEBIT CARD")
-            print("(3)","\t"*2,"NET BANKING")
-            print("(4)","\t"*2,"UPI PAYMENT")
+            print("[1]","\t"*2,"NET BANKING")
+            print("[2]","\t"*2,"DEBIT CARD")
+            print("[3]","\t"*2,"UPI PAYMENT")
             print("-"*89)
-            print("(C)","\t"*2,"CANCEL PAYMENT")
+            print("[C]","\t"*2,"CANCEL PAYMENT")
             print("-"*89)
-            payment_input = input("PLEASE SELECT YOUR OPERATION: ").upper()
+            payment_input = input("PLEASE SELECT YOUR OPTION: ").upper()
             if payment_input == "C":
-                restaurent_Menu(res)
+                self.restaurant_menu(res)
                 break
             elif payment_input == "1" or payment_input == "2" or payment_input == "3" or payment_input == "4":
                 self.restaurant_dict[res] += 1
@@ -220,18 +225,17 @@ class Swiggy():
                 print("\n" + "ERROR: Invalid Input (",payment_input,"). Try again!",sep='')
     
     
-   
     def payment_Status(self,res):
         
         while True:
             print("\n")
-            print("#"*39,"PAYMENT STATUS","#"*39)
+            print("*" * 39,"PAYMENT STATUS","*" * 39)
             print("\n"*3)
             print("\t"*4,"PAYMENT SUCCESSFUL")
             print("\n"*3)
             print("-"*89)
-            print("(M)","\t","BACK TO MAIN MENU")
-            print("-"*89)
+            print("[M]","\t","BACK TO MAIN MENU")
+            print("-" * 89)
             def endHotelProcess(self,res):
                 while(self.restaurant_dict[res]!=0):
                     time.sleep(100)
@@ -239,26 +243,66 @@ class Swiggy():
             t1 = threading.Thread(target=endHotelProcess,args=(self,res,))
             queueList.append(t1)
             queueList[-1].start()
-            payment_Status_input = input("PLEASE SELECT YOUR OPERATION: ").upper()
+            payment_Status_input = input("PLEASE SELECT YOUR OPTION: ").upper()
             if payment_Status_input == "M":
                 self.mainMenu()
                 break
             else:
                 print("\n" + "ERROR: Invalid Input (",payment_Status_input,"). Try again!",sep='')
     
+    def search_Menu(self):
+        l = []
+        flag = 0
+        while True:
+            print("\n")
+            print("*" * 36,"SEARCH MENU","*" * 35)
+            print("\n"*2)
+            print("[B] BACK TO ORDER FOOD PAGE")
+            print("-"*89)
+            dish = (input("PLEASE ENTER DISH NAME : ").upper())
+            sr_no = 1
+            for i,j in self.menu_restaurant.items():
+                for k,v in j.items():               #for dish and price in restaurant
+                    if k.startswith(dish):          # if dish starts with user input
+                        print("\n")
+                        print("-"*89)
+                        print("[",sr_no,"]"," ",i,sep="")
+                        sr_no += 1
+                        flag += 1
+                        l.append(i)
+                        print("DISH NAME: ",k,"\t"*6,"PRICE: ",v)
+                        print("-"*89)
+            if flag == 0:
+                print("\n" + "ENTERED DISH (",dish,") NOT FOUND. TRY AGAIN!",sep='')
+                continue  
+            else:                  
+                while True:                              #loop will run again until user inputs valid option
+                    try:                                  # exceptional error handling for if the input entered is not integer
+                        search_menu_input = input("PLEASE SELECT RESTAURANT: ")
+                        search_menu_input = int(search_menu_input)
+                        break
+                    except ValueError:
+                        print("NO VALID INPUT!! PLEASE TRY AGAIN ...")
+                if l != []:                                  
+                    self.restaurant_menu(l[search_menu_input-1])     # if list of the search result is not empty, this will call restaurent_Menu function with reference to user defined choice
+                    break
+                else:
+                    print("\n" + "ENTERED DISH [",dish,"] NOT FOUND. PLEASE TRY AGAIN!",sep='')
+                
+
     def joinServices(self):
         while True:
             print("\n")
-            print("#"*35,"JOIN OUR SERVICES","#"*35)
+            print("#"*35,"OTHER SERVICES","#"*35)
             print("\n")
             print("-"*89)
-            print("(1) NEW USER")
-            print("(2) EXSISTING USER")
+            print("[1] NEW USER-SIGN UP")
+            print("[2] EXISTING USER-LOGIN")
             print("\n"*2)
             print("-"*89)
-            print("(M) BACK TO MAIN MENU")
+            print("[M] BACK TO MAIN MENU")
             print("-"*89)
-            joinServices_input = input("PLEASE SELECT YOUR OPERATION: ").upper()
+            joinServices_input = input("PLEASE SELECT YOUR OPTION: ").upper()
             if joinServices_input == "M":
                 self.mainMenu()
                 break
@@ -277,7 +321,7 @@ class Swiggy():
             print("#"*32,"COMPLETE YOUR REGISTRATION PROCESS","#"*32)
             print("\n")
             print("-"*89)
-            print("(J) BACK TO JOIN SERVICES PAGE")
+            print("[J] BACK TO JOIN SERVICES PAGE")
             print("-"*89)
             new_user_id = input("PLEASE ENTER YOUR USERNAME: \n")
             new_user_pwd = int(input("PLEASE ENTER YOUR PASSWORD: \n"))
@@ -295,7 +339,7 @@ class Swiggy():
                     dish = (input("PLEASE ENTER YOUR DISH NAME TO BE ADDED IN MENU: \n").upper())
                     price = int(input("PLEASE ENTER THE AMOUNT FOR THE ABOVE DISH: \n"))
                     print("-"*89)
-                    print("(A) ADD MORE ITEMS","\t"*7,"(D) DONE ADDING ITEMS")
+                    print("[A] ADD MORE ITEMS","\t"*7,"[D] DONE ADDING ITEMS")
                     new_user_add_menu = input("PLEASE SELECT OPERATION TO BE PERFORMED: \n").upper()
                     dish_price.setdefault(dish,price)
                     self.menu_restaurant[new_user_restaurant] = dish_price
@@ -318,7 +362,7 @@ class Swiggy():
             print("#"*32,"WELCOME TO SWIGGY SERVICES","#"*32)
             print("\n"*3)
             print("-"*89)
-            print("(T) TRY AGAIN","\t"*4,"(M) BACK TO MAIN MENU")
+            print("[T] TRY AGAIN","\t"*4,"[M] BACK TO MAIN MENU")
             print("-"*89)
             exsisting_user_id = input("PLEASE ENTER USERNAME: \n")
             exsisting_user_pwd = int(input("PLEASE ENTER PASSWORD: \n"))
@@ -352,7 +396,7 @@ class Swiggy():
                     i += 1
                     print("(",i,")"," ",r,"\t"*7,v,sep="")
                 print("-"*89)
-                print("(A) ADD NEW DISH","\t"*2,"(M) BACK TO MAIN MENU","\t"*2,"(R) REMOVE DISH")
+                print("[A] ADD NEW DISH","\t"*2,"[M] BACK TO MAIN MENU","\t"*2,"[R] REMOVE DISH")
                 print("-"*89)
                 update_menu_input = input("PLEASE ENTER DISH NAME TO BE UPDATED OR SELECT ANY OPERATION TO BE PERFORMED: \n").upper()                      
                 if update_menu_input in self.menu_restaurant[k]:
@@ -381,7 +425,7 @@ class Swiggy():
             if dish not in self.menu_restaurant[k]:
                 self.menu_restaurant[k][dish]=price
             print("-"*89)
-            print("(A) ADD MORE ITEMS","\t"*6,"(D) DONE ADDING ITEMS")
+            print("[A] ADD MORE ITEMS","\t"*6,"[D] DONE ADDING ITEMS")
             add_dish_input = input("PLEASE SELECT OPERATION TO BE PERFORMED: \n").upper()
             if add_dish_input == "D":
                 print("\n"*2)
@@ -392,31 +436,30 @@ class Swiggy():
             elif add_dish_input != "A":
                 print("PLEASE ENTER VALID INPUT!!")
 
-def remove_Dish(k):
-    while True:
-        print("\n")
-        print("#"*20,"PLEASE ENTER NAME OF THE DISH TO BE REMOVED","#"*20)
-        print("\n"*2)
-        print("-"*89)
-        print("(T) TRY AGAIN","\t"*5,"(B) BACK TO UPDATE MENU PAGE")
-        print("-"*89)
-        dish = (input("PLEASE ENTER YOUR DISH NAME TO BE REMOVED: \n").upper())
-        if dish in menu_restaurent[k]:
-            del menu_restaurent[k][dish]
-            self.update_Menu(k)
-            break
-        else:
-            print("\n" + "ERROR: (",dish,"). not found in Menu. Try again!",sep='')
-        print("-"*89)
-        remove_dish_input = input("ENTER OPERATION TO BE PERFORMED: \n").upper()
-        if remove_dish_input == "B":
-            self.update_Menu(k)
-            break
-        elif remove_dish_input != "T":
-            print("\n" + "ERROR: Invalid Input (",remove_dish_input,"). Try again!",sep='')
+    def remove_Dish(self,k):
+            while True:
+                print("\n")
+                print("#"*20,"PLEASE ENTER NAME OF THE DISH TO BE REMOVED","#"*20)
+                print("\n"*2)
+                print("-"*89)
+                print("[T] TRY AGAIN","\t"*5,"[B] BACK TO UPDATE MENU PAGE")
+                print("-"*89)
+                dish = (input("PLEASE ENTER YOUR DISH NAME TO BE REMOVED: \n").upper())
+                if dish in self.menu_restaurant[k]:
+                    del self.menu_restaurant[k][dish]
+                    self.update_Menu(k)
+                    break
+                else:
+                    print("\n" + "ERROR: (",dish,"). not found in Menu. Try again!",sep='')
+                print("-"*89)
+                remove_dish_input = input("ENTER OPERATION TO BE PERFORMED: \n").upper()
+                if remove_dish_input == "B":
+                    self.update_Menu(k)
+                    break
+                elif remove_dish_input != "T":
+                    print("\n" + "ERROR: Invalid Input (",remove_dish_input,"). Try again!",sep='')
 
-
-            
+    
 if __name__ == "__main__":
     s=Swiggy()                                   
     s.mainMenu()
